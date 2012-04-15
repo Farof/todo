c650095e6e5233f623fe52fc7ce9ce5c5dd9b344
Farof <gfarof@gmail.com>
2012-04-15 18:00:53 +0200

    frisbee 0.2.1

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 #!/usr/bin/env /usr/local/bin/node
 
-var frisbee = require('./script/lib/frisbee/');
+var http, frisbee, app, server, PORT;
 
-var server = frisbee()
-              .use(frisbee.static({ folder: __dirname,
-                                    cache: true }))
-              .listen(8001);
+http    = require('http');
+frisbee = require('./script/lib/frisbee/');
+
+
+/****** CONF ******/
+
+PORT = 8001;
+
+/**** END CONF ****/
+
+
+/****** APP *******/
+app = frisbee()
+        .use(frisbee.static({ folder: __dirname,
+                              cache: true,
+                              expires: 1000 * 60 * 60 * 24 }));
+
+
+/*** INSTANCIATE ***/
+server = http.createServer(app);
+server.listen(PORT);
